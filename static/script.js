aa1c7f3d7155f997db79a7b695ddcb8b16821f36
Kavya P <79361494+krail-123@users.noreply.github.com>
2024-03-28 16:55:54 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,6 @@
+// Get the value of the textarea and store it in a variable
+function getTestingData() {
+    var testData = document.getElementById("test-data-input").value;
+    console.log("Testing Data:", testData);
+    // You can further process or manipulate the testing data here
+}
